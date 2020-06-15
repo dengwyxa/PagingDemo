189149603a609038ec5f58be8f571023cd2dd000
dengwy <37430935+dengwyxa@users.noreply.github.com>
2020-06-15 19:40:30 +0900

    Paging Demo

---
+++ app/src/main/java/com/example/pagingdemo/MainActivity.java
@@ -0,0 +1,113 @@
+package com.example.pagingdemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.Observer;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Button buttonPopulate, buttonClear;
+    StudentDao studentDao;
+    StudentsDatabase studentsDatabase;
+    PagedAdapter pagedAdapter;
+    LiveData<PagedList<Student>> allStudentsLivePaged;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        pagedAdapter = new PagedAdapter();
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.setAdapter(pagedAdapter);
+
+        studentsDatabase = StudentsDatabase.getInstance(this);
+        studentDao = studentsDatabase.getStudentDao();
+        allStudentsLivePaged = new LivePagedListBuilder<>(studentDao.getAllStudents(), 20).build();
+        allStudentsLivePaged.observe(this, new Observer<PagedList<Student>>() {
+            @Override
+            public void onChanged(final PagedList<Student> students) {
+                pagedAdapter.submitList(students);
+                students.addWeakCallback(null, new PagedList.Callback() {
+                    @Override
+                    public void onChanged(int position, int count) {
+                        Log.d("myLog", "onChanged:" + students);
+                    }
+
+                    @Override
+                    public void onInserted(int position, int count) {
+
+                    }
+
+                    @Override
+                    public void onRemoved(int position, int count) {
+
+                    }
+                });
+            }
+        });
+
+        buttonPopulate = findViewById(R.id.buttonPopulate);
+        buttonClear = findViewById(R.id.buttonClear);
+
+        buttonPopulate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Student[] students = new Student[1000];
+                for (int i = 0; i < 1000; i++) {
+                    Student student = new Student();
+                    student.setStudentNumber(i + 1);
+                    students[i] = student;
+                }
+                new InsertAsyncTask(studentDao).execute(students);
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new ClearAsyncTask(studentDao).execute();
+            }
+        });
+    }
+
+    static class InsertAsyncTask extends AsyncTask<Student, Void, Void> {
+        StudentDao studentDao;
+
+        public InsertAsyncTask(StudentDao studentDao) {
+            this.studentDao = studentDao;
+        }
+
+        @Override
+        protected Void doInBackground(Student... students) {
+            studentDao.insertStudents(students);
+            return  null;
+        }
+    }
+
+    static class ClearAsyncTask extends AsyncTask<Void, Void, Void> {
+        StudentDao studentDao;
+
+        public ClearAsyncTask(StudentDao studentDao) {
+            this.studentDao = studentDao;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            studentDao.deleteALlStudents();
+            return null;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/pagingdemo/PagedAdapter.java
@@ -0,0 +1,56 @@
+package com.example.pagingdemo;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.paging.PagedListAdapter;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PagedAdapter extends PagedListAdapter<Student, PagedAdapter.MyViewHolder> {
+
+    public PagedAdapter() {
+        super(new DiffUtil.ItemCallback<Student>() {
+            @Override
+            public boolean areItemsTheSame(@NonNull Student oldItem, @NonNull Student newItem) {
+                return oldItem.getId() == newItem.getId();
+            }
+
+            @Override
+            public boolean areContentsTheSame(@NonNull Student oldItem, @NonNull Student newItem) {
+                return oldItem.getStudentNumber() == newItem.getStudentNumber();
+            }
+        });
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.cell, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        Student student = getItem(position);
+        if (student == null) {
+            holder.textView.setText("读取中...");
+        } else {
+            holder.textView.setText(String.valueOf(student.getStudentNumber()));
+        }
+    }
+
+
+    static class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView textView;
+
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            this.textView = itemView.findViewById(R.id.textView);
+        }
+    }
+}
